879b1c2c5acab45e7081bb4e286c97ea41e230f6
Olaf Kober <olaf.kober@outlook.com>
2018-05-23 17:39:54 +0200

    added debug-app project

---
+++ src/Debug.InlayTester.Drivers.FeigReader/Program.cs
@@ -0,0 +1,63 @@
+﻿/* MIT License
+ * 
+ * Copyright (c) 2018, Olaf Kober
+ * https://github.com/Amarok79/InlayTester.Drivers.FeigReader
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+using System;
+using System.Threading.Tasks;
+using InlayTester.Drivers.Feig;
+using InlayTester.Shared.Transports;
+
+
+namespace InlayTester
+{
+	public static class Program
+	{
+		public static async Task Main()
+		{
+			var settings = new FeigReaderSettings {
+				TransportSettings = new SerialTransportSettings {
+					PortName = "COM4",
+					Baud = 38400,
+					DataBits = 8,
+					Parity = Parity.Even,
+					StopBits = StopBits.One,
+					Handshake = Handshake.None,
+				},
+				Address = 255,
+				Protocol = FeigProtocol.Advanced,
+				Timeout = TimeSpan.FromMilliseconds(1000)
+			};
+
+			using (var reader = FeigReader.Create(settings))
+			{
+				reader.Open();
+
+				var result = await reader.Transfer(FeigCommand.BaudRateDetection)
+					.ConfigureAwait(false);
+
+				if (result.Status != FeigTransferStatus.Success)
+					Console.WriteLine(result.Status);
+			}
+		}
+	}
+}
