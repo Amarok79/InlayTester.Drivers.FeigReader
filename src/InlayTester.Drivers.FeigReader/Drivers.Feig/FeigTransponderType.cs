44972b02712913140814d3b9c6213e7e82f18cba
Olaf Kober <olaf.kober@outlook.com>
2018-06-05 14:47:14 +0200

    added FeigTransponderType

---
+++ src/InlayTester.Drivers.FeigReader/Drivers.Feig/FeigTransponderType.cs
@@ -0,0 +1,101 @@
+﻿/* MIT License
+ * 
+ * Copyright (c) 2018, Olaf Kober
+ * https://github.com/Amarok79/InlayTester.Drivers.FeigReader
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+using System;
+
+
+namespace InlayTester.Drivers.Feig
+{
+	/// <summary>
+	/// This enumeration lists well-known transponder types.
+	/// </summary>
+	public enum FeigTransponderType : Byte
+	{
+		/// <summary>
+		/// 00h I-Code1
+		/// </summary>
+		ICode1 = 0x00,
+
+		/// <summary>
+		/// </summary>
+		/// 03h ISO15693
+		ISO15693 = 0x03,
+
+		/// <summary>
+		/// 04h ISO14443A
+		/// </summary>
+		ISO14443A = 0x04,
+
+		/// <summary>
+		/// </summary>
+		/// 05h ISO14443B
+		ISO14443B = 0x05,
+
+		/// <summary>
+		/// 06h I-Code EPC
+		/// </summary>
+		ICodeEPC = 0x06,
+
+		/// <summary>
+		/// 08h Jewel
+		/// </summary>
+		Jewel = 0x08,
+
+		/// <summary>
+		/// 09h ISO18000-3M3
+		/// </summary>
+		ISO18000_3M3 = 0x09,
+
+		/// <summary>
+		/// 0Ah SR176
+		/// </summary>
+		SR176 = 0x0A,
+
+		/// <summary>
+		/// 0Bh SRIxx (SRI512, SRIX512, SRI4K, SRIX4K)
+		/// </summary>
+		SRIxx = 0x0B,
+
+		/// <summary>
+		/// 10h Innovatron (14443-B’)
+		/// </summary>
+		Innovatron = 0x10,
+
+		/// <summary>
+		/// 11h CTx
+		/// </summary>
+		CTx = 0x11,
+
+		/// <summary>
+		/// 84h EPC class 1 Gen 2
+		/// </summary>
+		EPC_Class1_Gen2 = 0x084,
+
+
+		/// <summary>
+		/// FFh Unknown Transponder Type
+		/// </summary>
+		Unknown = 0xFF,
+	}
+}
---
+++ src/Tests.InlayTester.Drivers.FeigReader/Drivers.Feig/Test_FeigTransponderType.cs
@@ -0,0 +1,87 @@
+﻿/* MIT License
+ * 
+ * Copyright (c) 2018, Olaf Kober
+ * https://github.com/Amarok79/InlayTester.Drivers.FeigReader
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+using System;
+using NFluent;
+using NUnit.Framework;
+
+
+namespace InlayTester.Drivers.Feig
+{
+	[TestFixture]
+	public class Test_FeigTransponderType
+	{
+		[Test]
+		public void TestNames()
+		{
+			Check.That(Enum.GetNames(typeof(FeigTransponderType)))
+				.IsOnlyMadeOf(
+					nameof(FeigTransponderType.Unknown),
+					nameof(FeigTransponderType.CTx),
+					nameof(FeigTransponderType.EPC_Class1_Gen2),
+					nameof(FeigTransponderType.ICode1),
+					nameof(FeigTransponderType.ICodeEPC),
+					nameof(FeigTransponderType.Innovatron),
+					nameof(FeigTransponderType.ISO14443A),
+					nameof(FeigTransponderType.ISO14443B),
+					nameof(FeigTransponderType.ISO15693),
+					nameof(FeigTransponderType.ISO18000_3M3),
+					nameof(FeigTransponderType.Jewel),
+					nameof(FeigTransponderType.SR176),
+					nameof(FeigTransponderType.SRIxx)
+				);
+		}
+
+		[Test]
+		public void TestValues()
+		{
+			Check.That((Byte)FeigTransponderType.Unknown)
+				.IsEqualTo(0xFF);
+			Check.That((Byte)FeigTransponderType.CTx)
+				.IsEqualTo(0x11);
+			Check.That((Byte)FeigTransponderType.EPC_Class1_Gen2)
+				.IsEqualTo(0x84);
+			Check.That((Byte)FeigTransponderType.ICode1)
+				.IsEqualTo(0x00);
+			Check.That((Byte)FeigTransponderType.ICodeEPC)
+				.IsEqualTo(0x06);
+			Check.That((Byte)FeigTransponderType.Innovatron)
+				.IsEqualTo(0x10);
+			Check.That((Byte)FeigTransponderType.ISO14443A)
+				.IsEqualTo(0x04);
+			Check.That((Byte)FeigTransponderType.ISO14443B)
+				.IsEqualTo(0x05);
+			Check.That((Byte)FeigTransponderType.ISO15693)
+				.IsEqualTo(0x03);
+			Check.That((Byte)FeigTransponderType.ISO18000_3M3)
+				.IsEqualTo(0x09);
+			Check.That((Byte)FeigTransponderType.Jewel)
+				.IsEqualTo(0x08);
+			Check.That((Byte)FeigTransponderType.SR176)
+				.IsEqualTo(0x0A);
+			Check.That((Byte)FeigTransponderType.SRIxx)
+				.IsEqualTo(0x0B);
+		}
+	}
+}
