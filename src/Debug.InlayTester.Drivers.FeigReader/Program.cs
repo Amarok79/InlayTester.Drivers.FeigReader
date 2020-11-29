04dd99705aaad8d5141f96e0fd3f9c13c188b8c2
Olaf Kober <olaf.kober@outlook.com>
2020-11-29 18:19:36 +0100

    reformatted

--- src/Debug.InlayTester.Drivers.FeigReader/Program.cs
+++ src/Debug.InlayTester.Drivers.FeigReader/Program.cs
@@ -1,6 +1,6 @@
 ﻿/* MIT License
  * 
- * Copyright (c) 2019, Olaf Kober
+ * Copyright (c) 2020, Olaf Kober
  * https://github.com/Amarok79/InlayTester.Drivers.FeigReader
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -20,7 +20,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
-*/
+ */
 
 using System;
 using System.Diagnostics;
@@ -35,136 +35,130 @@ using InlayTester.Shared.Transports;
 
 namespace InlayTester
 {
-	public static class Program
-	{
-		public static async Task Main()
-		{
-			var config = new LogConfiguration() {
-				FactoryAdapter = new FactoryAdapterConfiguration() {
-					Type = typeof(NLogLoggerFactoryAdapter).AssemblyQualifiedName,
-					Arguments = new NameValueCollection() {
-						{ "configType", "FILE" },
-						{ "configFile", "./nlog.config" }
-					}
-				}
-			};
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            var config = new LogConfiguration {
+                FactoryAdapter = new FactoryAdapterConfiguration {
+                    Type = typeof(NLogLoggerFactoryAdapter).AssemblyQualifiedName,
+                    Arguments = new NameValueCollection {
+                        { "configType", "FILE" }, { "configFile", "./nlog.config" },
+                    },
+                },
+            };
 
-			LogManager.Configure(config);
+            LogManager.Configure(config);
 
-			var log = LogManager.GetLogger("Feig");
-			//var log = new NoOpLogger();
+            var log = LogManager.GetLogger("Feig");
 
-			var settings = new FeigReaderSettings() {
-				TransportSettings = new SerialTransportSettings() {
-					PortName = "COM3",
-					Baud = 38400,
-					DataBits = 8,
-					Parity = Parity.Even,
-					StopBits = StopBits.One,
-					Handshake = Handshake.None,
-				},
-				Address = 255,
-				Protocol = FeigProtocol.Advanced,
-				Timeout = TimeSpan.FromMilliseconds(1500),
-			};
+            //var log = new NoOpLogger();
 
-			using (IFeigReader reader = FeigReader.Create(settings, log))
-			{
-				reader.Open();
+            var settings = new FeigReaderSettings {
+                TransportSettings = new SerialTransportSettings {
+                    PortName  = "COM3",
+                    Baud      = 38400,
+                    DataBits  = 8,
+                    Parity    = Parity.Even,
+                    StopBits  = StopBits.One,
+                    Handshake = Handshake.None,
+                },
+                Address  = 255,
+                Protocol = FeigProtocol.Advanced,
+                Timeout  = TimeSpan.FromMilliseconds(1500),
+            };
 
-				var sw = new Stopwatch();
+            using (var reader = FeigReader.Create(settings, log))
+            {
+                reader.Open();
 
-
-				//await reader.ResetConfigurations(FeigBlockLocation.EEPROM)
-				//	.ConfigureAwait(false);
-
-				//await reader.ResetCPU()
-				//	.ConfigureAwait(false);
-
-				var cfg1 = await reader.ReadConfiguration(1, FeigBlockLocation.RAM)
-					.ConfigureAwait(false);
-				cfg1.Buffer[cfg1.Offset + 7] = 10;    // TR-RESPONSE-TIME: 1 sec
-				await reader.WriteConfiguration(1, FeigBlockLocation.RAM, cfg1)
-					.ConfigureAwait(false);
-
-				var cfg3 = await reader.ReadConfiguration(3, FeigBlockLocation.RAM)
-					.ConfigureAwait(false);
-				cfg3.Buffer[cfg3.Offset + 0] = 0x00;
-				cfg3.Buffer[cfg3.Offset + 1] = 0x10;
-				await reader.WriteConfiguration(3, FeigBlockLocation.RAM, cfg3)
-					.ConfigureAwait(false);
+                var sw = new Stopwatch();
 
 
-				for (Int32 i = 0; i < 1000000; i++)
-				{
-					if (i % 1000 == 0)
-						Console.WriteLine(i);
+                //await reader.ResetConfigurations(FeigBlockLocation.EEPROM)
+                //	.ConfigureAwait(false);
 
-					try
-					{
-						sw.Restart();
+                //await reader.ResetCPU()
+                //	.ConfigureAwait(false);
 
-						try
-						{
-							await reader.SwitchRF(0x00)
-								.ConfigureAwait(false);
-							await reader.SwitchRF(0x01)
-								.ConfigureAwait(false);
+                var cfg1 = await reader.ReadConfiguration(1, FeigBlockLocation.RAM).ConfigureAwait(false);
+                cfg1.Buffer[cfg1.Offset + 7] = 10; // TR-RESPONSE-TIME: 1 sec
+                await reader.WriteConfiguration(1, FeigBlockLocation.RAM, cfg1).ConfigureAwait(false);
 
-							sw.Stop();
-
-							//if (result.Response.Status != FeigStatus.OK)
-							//	Debugger.Launch();
-
-							//Console.WriteLine(result.Response.Status + " " + FeigTransponder.ToString(result.Transponders));
-						}
-						catch (FeigException exception)
-						{
-							Console.WriteLine(exception.Response.Status);
-							Debugger.Launch();
-						}
-						catch (TimeoutException)
-						{
-							Console.WriteLine("TIMEOUT");
-							Debugger.Launch();
-						}
+                var cfg3 = await reader.ReadConfiguration(3, FeigBlockLocation.RAM).ConfigureAwait(false);
+                cfg3.Buffer[cfg3.Offset + 0] = 0x00;
+                cfg3.Buffer[cfg3.Offset + 1] = 0x10;
+                await reader.WriteConfiguration(3, FeigBlockLocation.RAM, cfg3).ConfigureAwait(false);
 
 
-						//	var info = await reader.GetSoftwareInfo()
-						//		.ConfigureAwait(false);
+                for (var i = 0; i < 1000000; i++)
+                {
+                    if (i % 1000 == 0)
+                        Console.WriteLine(i);
 
-						//	var result = await reader.TestCommunication()
-						//		.ConfigureAwait(false);
+                    try
+                    {
+                        sw.Restart();
 
-						//	if (!result)
-						//		Debugger.Launch();
+                        try
+                        {
+                            await reader.SwitchRF(0x00).ConfigureAwait(false);
+                            await reader.SwitchRF(0x01).ConfigureAwait(false);
 
-						//	await reader.ResetRF()
-						//		.ConfigureAwait(false);
+                            sw.Stop();
 
-						//	BufferSpan cfg;
+                            //if (result.Response.Status != FeigStatus.OK)
+                            //	Debugger.Launch();
 
-						//	cfg = await reader.ReadConfiguration(1, FeigBlockLocation.RAM)
-						//		.ConfigureAwait(false);
-						//	cfg = await reader.ReadConfiguration(1, FeigBlockLocation.EEPROM)
-						//		.ConfigureAwait(false);
+                            //Console.WriteLine(result.Response.Status + " " + FeigTransponder.ToString(result.Transponders));
+                        }
+                        catch (FeigException exception)
+                        {
+                            Console.WriteLine(exception.Response.Status);
+                            Debugger.Launch();
+                        }
+                        catch (TimeoutException)
+                        {
+                            Console.WriteLine("TIMEOUT");
+                            Debugger.Launch();
+                        }
 
-						//	cfg.Buffer[cfg.Offset + 0] = 0xCD;
 
-						//	await reader.WriteConfiguration(1, FeigBlockLocation.RAM, cfg)
-						//		.ConfigureAwait(false);
+                        //	var info = await reader.GetSoftwareInfo()
+                        //		.ConfigureAwait(false);
 
-						//	await reader.ResetConfigurations(FeigBlockLocation.RAM)
-						//		.ConfigureAwait(false);
+                        //	var result = await reader.TestCommunication()
+                        //		.ConfigureAwait(false);
 
-						File.AppendAllText("d:\\test.txt", sw.ElapsedMilliseconds + "\r\n");
-					}
-					catch (Exception)
-					{
-						Debugger.Launch();
-					}
-				}
-			}
-		}
-	}
+                        //	if (!result)
+                        //		Debugger.Launch();
+
+                        //	await reader.ResetRF()
+                        //		.ConfigureAwait(false);
+
+                        //	BufferSpan cfg;
+
+                        //	cfg = await reader.ReadConfiguration(1, FeigBlockLocation.RAM)
+                        //		.ConfigureAwait(false);
+                        //	cfg = await reader.ReadConfiguration(1, FeigBlockLocation.EEPROM)
+                        //		.ConfigureAwait(false);
+
+                        //	cfg.Buffer[cfg.Offset + 0] = 0xCD;
+
+                        //	await reader.WriteConfiguration(1, FeigBlockLocation.RAM, cfg)
+                        //		.ConfigureAwait(false);
+
+                        //	await reader.ResetConfigurations(FeigBlockLocation.RAM)
+                        //		.ConfigureAwait(false);
+
+                        File.AppendAllText("d:\\test.txt", sw.ElapsedMilliseconds + "\r\n");
+                    }
+                    catch (Exception)
+                    {
+                        Debugger.Launch();
+                    }
+                }
+            }
+        }
+    }
 }
